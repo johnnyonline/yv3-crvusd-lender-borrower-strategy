973bc604cc40f612fc8a9e9739de39cef8471739
johnnyonline <johnnyonline.eth@gmail.com>
2025-09-15 19:16:14 -0300

    chore: update base

--- src/BaseLenderBorrower.sol
+++ src/BaseLenderBorrower.sol
@@ -917,7 +917,8 @@ abstract contract BaseLenderBorrower is BaseHealthCheck {
     function _emergencyWithdraw(
         uint256 _amount
     ) internal virtual override {
-        if (_amount > 0) _withdrawBorrowToken(Math.min(_amount, _lenderMaxWithdraw()));
+        _amount = Math.min(_amount, _lenderMaxWithdraw());
+        if (_amount > 0) _withdrawBorrowToken(_amount);
 
         // Repay everything we can.
         _repayTokenDebt();
